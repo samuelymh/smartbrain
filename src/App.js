a87b2db9a1446f4831fbeeccf9daf4743a4b3eef
samuelymh <103726434+samuelymh@users.noreply.github.com>
2022-06-22 16:13:35 +0800

    feat: Implemented interactive background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,88 @@
 import './App.css';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+
+// Configurable particle settings
+const particlesOptions = {
+  interactivity: {
+    events: {
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      repulse: {
+        distance: 150,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outModes: {
+        default: "bounce",
+      },
+      random: false,
+      speed: 5.5, // Controls speed of circles.
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 600, // The higher, the more lines.
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: { min: 1, max: 5 },
+    },
+  },
+}
 
 function App() {
+  const particlesInit = async (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
     <div className="App">
+      <Particles id='tsparticles' init={particlesInit} loaded={particlesLoaded} options={particlesOptions}/>
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
