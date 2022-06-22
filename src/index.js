208dfd12c1f9920b8a104275f2d0c24dcc974052
samuelymh <103726434+samuelymh@users.noreply.github.com>
2022-06-22 15:51:16 +0800

    feat: Added comments about React.StrictMode

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
+// When wrapped in React.StrictMode, component runs twice.
+// First to check for errors, second as the primary render.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
