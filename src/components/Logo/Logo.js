4956fc33f3bfae8e4149e3eb6d09a04d89512545
samuelymh <103726434+samuelymh@users.noreply.github.com>
2022-06-23 14:30:02 +0800

    feat: Added shouldComponentUpdate lifecycle method in Logo component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Tilty from 'react-tilty';
 import brain from './brain.png';
 import './Logo.css';
 
-const Logo = () => {
-    return (
-        <div className="ma4 mt0">
-            <Tilty className="Tilt br2 shadow-2" options={{max: 55}} style={{height: 150, width: 150}}>
-                <div className='Tilt-inner pa3'>
-                    <img style={{paddingTop: '10px'}} src={brain} alt="brain logo" />
-                </div>
-            </Tilty>
-        </div>
-    );
+class Logo extends Component {
+    shouldComponentUpdate(){
+        console.log("Logo - shouldComponentUpdate lifecycle")
+        return false;
+    }
+
+    render(){
+        console.log("Logo - Render lifecycle")
+
+        return (
+            <div className="ma4 mt0">
+                <Tilty className="Tilt br2 shadow-2" options={{max: 55}} style={{height: 150, width: 150}}>
+                    <div className='Tilt-inner pa3'>
+                        <img style={{paddingTop: '10px'}} src={brain} alt="brain logo" />
+                    </div>
+                </Tilty>
+            </div>
+        )
+    }
 }
 
 export default Logo;
