16bb85611a624861bca3f70976ee0e4e0978b781
samuelymh <103726434+samuelymh@users.noreply.github.com>
2022-06-23 13:42:51 +0800

    feat: Implemented BoundingBoxes

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './FaceRecognition.css';
+import BoundingBoxes from '../BoundingBoxes/BoundingBoxes'
 
-const FaceRecognition = ({ imageUrl, box }) => {
+const FaceRecognition = ({ imageUrl, boxArray }) => {
     return (
         <div className='center ma'>
             <div className='absolute mt2'>
                 <img id='inputimage' src={imageUrl} alt="" width='500px' height='auto' />
-                <div 
-                className='bounding-box' 
-                style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}>
-                </div>
+                <BoundingBoxes boxes={boxArray} />
             </div>
         </div>
     );
